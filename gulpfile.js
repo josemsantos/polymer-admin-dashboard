3f4382fda7d3b21ab6014ffdac3c387675a9c78d
José Santos <jose.santos@jovago.com>
2017-10-09 12:28:27 +0100

    Setup rootpath and basepath for custom urls

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,32 @@
 'use strict';
 
+const path = require('path');
 var gulp = require('gulp');
+const $ = require('gulp-load-plugins')();
 var replace = require('gulp-replace');
 var jMerge = require('gulp-merge-json');
 var inlineSource = require('gulp-inline-source');
 
 var params = require('./parameters.json');
+const precacheParams = require('./sw-precache-config.js');
+
+// Keep the global.config above any of the gulp-tasks that depend on it
+global.config = {
+    polymerJsonPath: path.join(process.cwd(), 'polymer.json'),
+    build: {
+      rootDirectory: 'build',
+      bundledDirectory: 'es5-bundled'
+    },
+    params: params,
+    // Path to your service worker, relative to the build root directory
+    serviceWorkerPath: 'service-worker.js',
+    // Service Worker precache options based on
+    // https://github.com/GoogleChrome/sw-precache#options-parameter
+    swPrecacheConfig: precacheParams
+  };
+  
+  // Build paths
+  global.config.bundledPath = path.join(global.config.build.rootDirectory, global.config.build.bundledDirectory);
 
 gulp.task('env', function() {
 
@@ -21,12 +42,10 @@ gulp.task('parameters', function() {
         .pipe(gulp.dest('./'));
 });
 
-// gulp.task('inlinesource', function() {
-//   var inlineBundle = gulp.src('/build/es5-bundled-server/index.html')
-//     .pipe(inlineSource())
-//     .pipe(gulp.dest('/build/es5-bundled-server/'));
-// 
-//   return inlineBundle;
-// });
+gulp.task('inlinesource', function() {
+    return gulp.src(global.config.bundledPath + '/index.html')
+                .pipe($.inlineSource())
+                .pipe(gulp.dest(global.config.bundledPath));
+});
 
-gulp.task('default', ['env', 'parameters']);
+gulp.task('default', ['parameters', 'env']);
