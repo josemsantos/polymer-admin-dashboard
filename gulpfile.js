e5537d931a7d040d5c8aaa5a0b87d093cbe6ee93
Luís Hermenegildo <luis.hermenegildo@africainternetgroup.com>
2017-10-10 13:55:12 +0100

    Add revision to files in order to invalidate cache on deploy

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var gulp = require('gulp');
 var replace = require('gulp-replace');
 var jMerge = require('gulp-merge-json');
 var inlineSource = require('gulp-inline-source');
+var path = require('path');
+var revAll = require('gulp-rev-all');
 
 var params = require('./parameters.json');
 
@@ -25,8 +27,23 @@ gulp.task('parameters', function() {
 //   var inlineBundle = gulp.src('/build/es5-bundled-server/index.html')
 //     .pipe(inlineSource())
 //     .pipe(gulp.dest('/build/es5-bundled-server/'));
-// 
+
 //   return inlineBundle;
 // });
+
+
+// Invalidates cache on each deploy
+// Rename hash files
+var revisionConfig = {
+  dontRenameFile: ['index.html', 'service-worker.js', 'robots.txt', 'favicon.ico', 'bower.json', /\/images\/.*/, /\/src\/_locales\/.*/],
+  dontUpdateReference: ['index.html', 'service-worker.js', 'robots.txt', 'favicon.ico', 'bower.json', /\/images\/.*/, /\/src\/_locales\/.*/]
+};
+
+gulp.task('revision', function() {
+  return gulp.src('build/es5-bundled-server' + '/**')
+    .pipe(revAll.revision(revisionConfig))
+    .pipe(gulp.dest('build/es5-bundled-server'))
+    .pipe(gulp.dest('build/dist'));
+});
 
 gulp.task('default', ['env', 'parameters']);
